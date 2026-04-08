0d3bb79cd82476d3417e0bf543da555aecca422d
Higor-cz <higorcz19@gmail.com>
2026-04-08 17:09:35 -0300

    Create 07.py

---
+++ Lista03/07.py
@@ -0,0 +1,11 @@
+#Faça um programa que leia 5 números e informe o maior número.
+i = 0
+numerom = True
+while i < 5:
+    i += 1
+    n = float(input(f'Digite o {i} número: '))
+    if numerom == True:
+        numerom = n
+    elif n > nm:
+        numerom = n
+print(numerom)
